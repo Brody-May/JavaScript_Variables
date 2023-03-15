2d5c7926a1fbad59e1c2522d693ca1c7d508e3a8
Brody May <brodymay2019@outlook.com>
2023-03-15 10:55:31 -0400

    Exercise complete

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+const firstName = "Brody";
+let lastName = "May";
+var age; 29
+
+lastName = "May";
+age = 29;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`)
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selctedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long. 
+Your last bookmark was page ${bookmark}.`
 );
-
-// Exercise 1
-
-// Exercise 2
-
-// Exercise 3
